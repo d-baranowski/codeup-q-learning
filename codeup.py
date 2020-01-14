b6368af0a266af6bfcd20412dde3e55a4552b48e
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2020-01-14 22:03:46 +0000

    stage-one-imports

---
+++ codeup.py
@@ -0,0 +1,6 @@
+# Import libraries
+import numpy as np # Numpy makes python do better at maths
+import gym # OpenAI Gym for our environment
+import random # Random number genrator to let the fate decide
+import time # Allows our program to sleep so things slow down a little
+
